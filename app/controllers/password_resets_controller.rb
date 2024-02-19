ca7032a12d19836d4d64317b9d0a23a4f4f81a49
sepidsoroush <s.soroush2012@gmail.com>
2024-02-19 12:58:42 +0200

    ✨ add "update forgotten password" feature

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,40 @@
+class PasswordResetsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    if @user.present?
+      # send email
+      PasswordMailer.with(user: @user).reset.deliver_later
+    end
+
+    redirect_to root_path,
+                notice:
+                  "If an account with that email was found, we have sent an email to reset your password."
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password-reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expired. Please try again."
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password-reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path,
+                  notice:
+                    "Your password was reset successfully. Please sign in."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+end
