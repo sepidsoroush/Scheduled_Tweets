b08b5b8eeba7431eb3610a08234feedd6b3506e0
sepidsoroush <s.soroush2012@gmail.com>
2024-02-27 09:35:02 +0200

    🦺 Tweet validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,9 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: { minimum: 1, maximum: 280 }
+  validates :publish_at, presence: true
+
+  after_initialize { self.publish_at ||= 3.hours.from_now }
 end
